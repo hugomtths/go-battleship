83f22b40083770712d962c9ccb0113522d7f6654
cauaof_souza <cauasantos064@gmail.com>
2026-02-21 16:46:24 -0300

    Remove arquivos desnecessários

--- UI/screen.go
+++
@@ -1,9 +0,0 @@
-package UI
-
-import "github.com/hajimehoshi/ebiten/v2"
-
-type Screen interface {
-    Update() error
-    Draw(screen *ebiten.Image)
-    Layout(outsideWidth, outsideHeight int) (int, int)
-}
